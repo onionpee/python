73c7b6608a040bfffd55224251000c7c460494cb
onionpee <107740868+onionpee@users.noreply.github.com>
2022-10-26 11:18:32 +0900

    Add files via upload

---
+++ enemy.py
@@ -0,0 +1,258 @@
+import time
+import os
+life = 10
+
+class Person:
+    name = '아이유'
+    global life
+    def 첫번째(self):
+        global life
+        print('93년생 가수이다.')
+        Pname = input('Enter 또는 정답 입력')
+        life -= 1
+        if Pname == '아이유':
+            print('정답!')
+            time.sleep(0.5)
+            os.system('"C:/Windows/System32/notepad.exe"')
+            exit(0)
+        print(f'남은 기회 : {life}')
+
+    def 두번째(self):
+        global life
+        print('카카오ENT 소속이다.')
+        Pname = input('Enter 또는 정답 입력')
+        life -= 1
+        if Pname == '아이유':
+            print('정답!')
+            time.sleep(0.5)
+            os.system('"C:/Windows/System32/notepad.exe"')
+            exit(0)
+        print(f'남은 기회 : {life}')
+
+    def 세번째(self):
+        global life
+        print('데일리 스포츠 선정 한국인이 좋아하는 연예인 5위안에 매번 기재된다.')
+        Pname = input('Enter 또는 정답 입력')
+        life -= 1
+        if Pname == '아이유':
+            print('정답!')
+            time.sleep(0.5)
+            os.system('"C:/Windows/System32/notepad.exe"')
+            exit(0)
+        print(f'남은 기회 : {life}')
+
+    def 네번째(self):
+        global life
+        print('팬들과의 소통이 활발한 연예인이다.')
+        Pname = input('Enter 또는 정답 입력')
+        life -= 1
+        if Pname == '아이유':
+            print('정답!')
+            time.sleep(0.5)
+            os.system('"C:/Windows/System32/notepad.exe"')
+            exit(0)
+        print(f'남은 기회 : {life}')
+
+    def 다섯번째(self):
+        global life
+        print('가요계 전체에서 음원이 가장 잘된 가수이다.')
+        Pname = input('Enter 또는 정답 입력')
+        life -= 1
+        if Pname == '아이유':
+            print('정답!')
+            time.sleep(0.5)
+            os.system('"C:/Windows/System32/notepad.exe"')
+            exit(0)
+        print(f'남은 기회 : {life}')
+
+    def 여섯번째(self):
+        global life
+        print('연기자로도 활동했다.')
+        Pname = input('Enter 또는 정답 입력')
+        life -= 1
+        if Pname == '아이유':
+            print('정답!')
+            time.sleep(0.5)
+            os.system('"C:/Windows/System32/notepad.exe"')
+            exit(0)
+        print(f'남은 기회 : {life}')
+
+    def 일곱번째(self):
+        global life
+        print('국세청 및 경찰청 홍보대사로 활동했다.')
+        Pname = input('Enter 또는 정답 입력')
+        life -= 1
+        if Pname == '아이유':
+            print('정답!')
+            time.sleep(0.5)
+            os.system('"C:/Windows/System32/notepad.exe"')
+            exit(0)
+        print(f'남은 기회 : {life}')
+
+    def 여덟번째(self):
+        global life
+        print('이름만으로도 하나의 "브랜드" 이자 "시대의 아이콘" 이란 평가를 받는다')
+        Pname = input('Enter 또는 정답 입력')
+        life -= 1
+        if Pname == '아이유':
+            print('정답!')
+            time.sleep(0.5)
+            os.system('"C:/Windows/System32/notepad.exe"')
+            exit(0)
+        print(f'남은 기회 : {life}')
+
+    def 아홉번째(self):
+        global life
+        print('세대와 상관없이 지지를 받는 가수이다.')
+        Pname = input('Enter 또는 정답 입력')
+        life -= 1
+        if Pname == '아이유':
+            print('정답!')
+            time.sleep(0.5)
+            os.system('"C:/Windows/System32/notepad.exe"')
+            exit(0)
+        print(f'남은 기회 : {life}')
+
+    def 열번째(self):
+        global life
+        print('"미아" 라는 곡으로 데뷔했다.')
+        Pname = input('Enter 또는 정답 입력')
+        life -= 1
+        if Pname == '아이유':
+            print('정답!')
+            time.sleep(0.5)
+            os.system('"C:/Windows/System32/notepad.exe"')
+            exit(0)
+        print(f'남은 기회 : {life}')
+        if life == 0:
+            print('실패!')
+            time.sleep(1)
+            exit(0)
+
+class person2:
+    name = '이상혁'
+    # life = 10
+    global life
+    def 첫번째(self):
+        global life
+        print('96년생 프로게이머이다.')
+        Pname = input('Enter 또는 정답 입력')
+        life -= 1
+        if Pname == '이상혁':
+            print('정답!')
+            time.sleep(0.5)
+            os.system('"C:/Windows/System32/notepad.exe"')
+            exit(0)
+        print(f'남은 기회 : {life}')
+
+    def 두번째(self):
+        global life
+        print('SKT T1 소속이다.')
+        Pname = input('Enter 또는 정답 입력')
+        life -= 1
+        if Pname == '이상혁':
+            print('정답!')
+            time.sleep(0.5)
+            os.system('"C:/Windows/System32/notepad.exe"')
+            exit(0)
+        print(f'남은 기회 : {life}')
+
+    def 세번째(self):
+        global life
+        print('E 스포츠 역사상 가장 위대한 선수로 평가받는다.')
+        Pname = input('Enter 또는 정답 입력')
+        life -= 1
+        if Pname == '이상혁':
+            print('정답!')
+            time.sleep(0.5)
+            os.system('"C:/Windows/System32/notepad.exe"')
+            exit(0)
+        print(f'남은 기회 : {life}')
+
+    def 네번째(self):
+        global life
+        print('주로 사용하는 챔피언은 "아지르", "르블랑", "라이즈" 이다.')
+        Pname = input('Enter 또는 정답 입력')
+        life -= 1
+        if Pname == '이상혁':
+            print('정답!')
+            time.sleep(0.5)
+            os.system('"C:/Windows/System32/notepad.exe"')
+            exit(0)
+        print(f'남은 기회 : {life}')
+
+    def 다섯번째(self):
+        global life
+        print('개인 방송 중 100만원 후원에도 신경쓰지 않는 사람이다.')
+        Pname = input('Enter 또는 정답 입력')
+        life -= 1
+        if Pname == '이상혁':
+            print('정답!')
+            time.sleep(0.5)
+            os.system('"C:/Windows/System32/notepad.exe"')
+            exit(0)
+        print(f'남은 기회 : {life}')
+
+    def 여섯번째(self):
+        global life
+        print('우승 상금이 16억이 넘는다.')
+        Pname = input('Enter 또는 정답 입력')
+        life -= 1
+        if Pname == '이상혁':
+            print('정답!')
+            time.sleep(0.5)
+            os.system('"C:/Windows/System32/notepad.exe"')
+            exit(0)
+        print(f'남은 기회 : {life}')
+
+    def 일곱번째(self):
+        global life
+        print('월드 챔피언쉽에서 3회 우승하였다.')
+        Pname = input('Enter 또는 정답 입력')
+        life -= 1
+        if Pname == '이상혁':
+            print('정답!')
+            time.sleep(0.5)
+            os.system('"C:/Windows/System32/notepad.exe"')
+            exit(0)
+        print(f'남은 기회 : {life}')
+
+    def 여덟번째(self):
+        global life
+        print('원클럽맨이다.')
+        Pname = input('Enter 또는 정답 입력')
+        life -= 1
+        if Pname == '이상혁':
+            print('정답!')
+            time.sleep(0.5)
+            os.system('"C:/Windows/System32/notepad.exe"')
+            exit(0)
+        print(f'남은 기회 : {life}')
+
+    def 아홉번째(self):
+        global life
+        print('hide on bush라는 닉네임을 사용한다.')
+        Pname = input('Enter 또는 정답 입력')
+        life -= 1
+        if Pname == '이상혁':
+            print('정답!')
+            time.sleep(0.5)
+            os.system('"C:/Windows/System32/notepad.exe"')
+            exit(0)
+        print(f'남은 기회 : {life}')
+
+    def 열번째(self):
+        global life
+        print('그 이름은 페이커이다.')
+        Pname = input('Enter 또는 정답 입력')
+        life -= 1
+        if Pname == '이상혁':
+            print('정답!')
+            time.sleep(0.5)
+            os.system('"C:/Windows/System32/notepad.exe"')
+            exit(0)
+        print(f'남은 기회 : {life}')
+        if life == 0:
+            print('실패!')
+            time.sleep(1)
+            exit(0)
